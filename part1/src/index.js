e2fc3908522a75bf1bb21e18379089b19b0497f9
Mael Machuca <100020513+MiniMahe@users.noreply.github.com>
2023-07-27 23:17:33 +0200

    update, part 1c

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,18 +1,42 @@
-import React from 'react'
+import React, {useState} from 'react'
 import ReactDOM from 'react-dom'
 
 
 const Header = (props) => {
-  return <h1>{props.course}</h1>
+  const {age,course} = props
+
+  const year = () => new Date().getFullYear() - age
+
+  return (
+    <div>
+      <h1>{course} and you are {age} years old</h1>
+      <h2>You were born in {year()}</h2>
+    </div>
+  )
 }
 
 const Content = (props) => {
   return <p>{props.name} {props.exercises}</p>
 }
 
-const App = () => {
+const Display = ({ counter }) => <div>{counter}</div>
+
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
+
+const App = (prop) => {
+  const [ counter, setCounter ] = useState(0)
+  
+  const increaseByOne = () => setCounter(counter + 1)
+  const decreaseByOne = () => setCounter(counter - 1)
+  const setToZero = () => setCounter(0)
+
   const props = {
     course: 'Half Stack application development',
+    age: 19,
     parts: [
       {
         name: 'Fundamentals of React',
@@ -30,12 +54,34 @@ const App = () => {
   }
   return (
     <div>
-      <Header course={props.course} />
+      <Display counter={counter}/>
+      <Button
+        handleClick={increaseByOne}
+        text='plus'
+      />
+      <Button
+        handleClick={setToZero}
+        text='zero'
+      />     
+      <Button
+        handleClick={decreaseByOne}
+        text='minus'
+      />   
+      <Header age={props.age} course={props.course} />
       <Content name={props.parts[0].name} exercises={props.parts[0].name} />
       <Content name={props.parts[1].name} exercises={props.parts[1].name} />
       <Content name={props.parts[2].name} exercises={props.parts[2].name} />
     </div>
   )
+  
 }
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+  <App />, 
+  document.getElementById('root')
+)
+
+//   setInterval(() => {
+//   refresh()
+//   counter += 1
+// }, 1000)
