fbad5bbbc28de83155a1d0123ebf4f64711d9c6d
Mael Machuca <100020513+MiniMahe@users.noreply.github.com>
2023-08-10 18:20:39 +0200

    Update index.js

--- part1/src/index.js
+++ part1/src/index.js
@@ -21,15 +21,46 @@ const Content = (props) => {
 
 const Display = ({ counter }) => <div>{counter}</div>
 
-const Button = ({ handleClick, text }) => (
-  <button onClick={handleClick}>
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return (
+      <div>
+        the app is used by pressing the buttons
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      button press history: {props.allClicks.join(' ')}
+    </div>
+  )
+}
+
+const Button = ({ onClick, text }) => (
+  <button onClick={onClick}>
     {text}
   </button>
 )
 
+
 const App = (prop) => {
   const [ counter, setCounter ] = useState(0)
-  
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
+
+  const handleLeftClick = () => {
+    allClicks.push('L')
+    setAll(allClicks)
+    setLeft(left + 1)
+  }
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    setRight(right + 1)
+  }
+
   const increaseByOne = () => setCounter(counter + 1)
   const decreaseByOne = () => setCounter(counter - 1)
   const setToZero = () => setCounter(0)
@@ -54,6 +85,13 @@ const App = (prop) => {
   }
   return (
     <div>
+      {left}
+      <button onClick={handleLeftClick}>left</button>
+      <button onClick={handleRightClick}>right</button>
+      {right}
+      <History allClicks={allClicks} />
+      <Button onClick={handleLeftClick} text='left' />
+      <Button onClick={handleRightClick} text='right' />
       <Display counter={counter}/>
       <Button
         handleClick={increaseByOne}
